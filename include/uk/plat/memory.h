e7c19117345323539bce0177fef4f4b2051004d7
Cason Schindler <casonjs@cs.utexas.edu>
2019-12-14 21:06:36 -0600

    include/uk/plat: Memory Ballooning API
    Add inflate and deflate API headers in memory.h.
    
    Signed-off-by: Jack Raney <raney.jack99@gmail.com>
    Signed-off-by: Cason Schindler <cason.j.schindler@gmail.com>

--- include/uk/plat/memory.h
+++ include/uk/plat/memory.h
@@ -164,6 +164,25 @@ struct uk_alloc *ukplat_memallocator_get(void);
  */
 void ukplat_stack_set_current_thread(void *thread_addr);
 
+/**
+ * Inflates the memory balloon by 1 extent. After this point, the extent
+ * starting at the specified virtual address will be unavailable to the guest.
+ * The host will then be able to use the extent for its own purposes.
+ * @param va The starting virtual address to release
+ * @param order The sie of the extent to be released
+ * @return >= 0 on success, < 0 otherwise.
+ */
+int ukplat_inflate(void *va, int order);
+
+/**
+ * Deflates the memory balloon by 1 extent, and returns a frame to the
+ * hypervisor starting at the specified virtual address.
+ * @param va The starting virtual address to recover
+ * @param order The size of the extent to be recovered
+ * @return num pages reclaimed on success, < 0 otherwise.
+ */
+int ukplat_deflate(void *va, int order);
+
 #ifdef __cplusplus
 }
 #endif
